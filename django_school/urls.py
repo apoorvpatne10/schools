c1e4971fd832f0233d41d012c267b45565107d41
apoorv <apoorv.patne10@gmail.com>
2018-11-02 23:24:37 +0530

    ..

--- django_school/urls.py
+++ django_school/urls.py
@@ -4,6 +4,7 @@ from classroom.views import classroom, students, teachers
 
 urlpatterns = [
     path('', include('classroom.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
     path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
